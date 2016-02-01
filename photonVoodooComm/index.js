3b2cfde605a213ef7b591bfb25f0f2e26f50b2d4
Ian Zhang <ianzhang1986@gmail.com>
2016-01-31 17:55:22 -0800

    [Feature] Add node server using particle IO to comm with Photon

---
+++ photonVoodooComm/index.js
@@ -0,0 +1,28 @@
+var Particle = require('particle-io');
+
+var objectDebug = function(obj) {
+  return JSON.stringify(obj);
+};
+
+/*var board = new Particle({
+  token: process.env.PARTICLE_TOKEN,
+  deviceId: process.env.PARTICLE_DEVICE_ID
+});*/
+
+var board = new Particle({
+  host: '192.168.1.117',
+  port: 48879
+});
+
+board.on('ready', function() {
+  console.log(
+    "Connected to " + board.deviceName + 
+    " (" + board.deviceId + ") " +
+    "at " + board.deviceIp + ":"  + board.devicePort
+  );
+
+  this.pinMode("D7", this.MODES.OUTPUT); // this.pinMode("D7", 1);
+  this.digitalWrite("D7", 1);
+});
+
+console.log("Board mode: " + objectDebug(board.MODES));
